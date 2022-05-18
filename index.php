d0c5c021a708124d6c72d8dfd6a7b036d3639cfa
n-esc4p <edvardradesey@gmail.com>
2022-05-18 09:31:23 +0200

    test

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head lang="fr" dir="ltr">
+        <meta charset="utf-8" />
+        <title>Commentaires</title>
+        <link rel="stylesheet" href="css/master.css" />
+    </head>
+    <body>
+        
+        <?php 
+        echo(1+1);
+        
+        ?>
+    </body>
+</html>
